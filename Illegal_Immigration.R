91eeabbd68fa1dc984fa238a9099007e7ebbaf92
bryandaetz <bryandaetz@umail.ucsb.edu>
2017-02-18 11:31:47 -0800

    Add files via upload

--- Illegal_Immigration.R
+++ Illegal_Immigration.R
@@ -22,13 +22,14 @@ arrests$Year <- as.integer(arrests$Year)
 write.csv(arrests, file = "immigration_arrests.csv")
 
 #creating a new dataframe with yearly arrest totals
+#it appears the original dataframe already included totals as observations where Border == United States
 totals <- arrests %>%
               group_by(Year, Type) %>%
-              summarise(Total_Arrests = sum(Number_Arrested, na.rm = TRUE)) %>%
+              filter(Border == "United States") %>%
               arrange(Type)
 
 #creating an area plot comparing yearly arrest totals of all immigrants and of only mexican immigrants 
-tot <- ggplot(totals, aes(x = Year, y = Total_Arrests, fill = Type)) +
+tot <- ggplot(totals, aes(x = Year, y = Number_Arrested, fill = Type)) +
           geom_area(alpha = 0.65, position = "dodge") +
           xlab("Year") +
           ylab("Total Arrests") +
@@ -36,6 +37,53 @@ tot <- ggplot(totals, aes(x = Year, y = Total_Arrests, fill = Type)) +
           theme_minimal()
 ggplotly(tot)
 
+
+#creating a new dataframe with yearly arrest totals by border
+#again the original dataframe already included this totals as observations where Sector == All
+by_border <- arrests %>%
+                  group_by(Year, Type) %>%
+                  filter(Sector == "All") %>%
+                  arrange(Type)
+
+#creating area plots for each border comparing the yearly arrest totals of all immigrants and of only mexicans
+borders <- ggplot(by_border, aes(x = Year, y = Number_Arrested, fill = Type)) +
+              geom_area(alpha = 0.65, position = "dodge") +
+              facet_wrap(~ Border) +
+              xlab("Year") +
+              ylab("Total Arrests") +
+              ggtitle("Illegal Immigration Arrests at Each Border") +
+              theme_bw()
+ggplotly(borders)
+
+#since the arrest totals are so much higher for the southwest than for the other two borders it may make more sense
+#   create individual graphs for each border instead of facet wrapping
+
+coast <- ggplot(filter(by_border, Border == "Coast"), aes(x = Year, y = Number_Arrested, fill = Type)) +
+              geom_area(alpha = 0.65, position = "dodge") +
+              xlab("Year") +
+              ylab("Total Arrests") +
+              ggtitle("Illegal Immigration Arrests Along the Coast") +
+              theme_minimal()
+ggplotly(coast)
+
+north <- ggplot(filter(by_border, Border == "North"), aes(x = Year, y = Number_Arrested, fill = Type)) +
+              geom_area(alpha = 0.65, position = "dodge") +
+              xlab("Year") +
+              ylab("Total Arrests") +
+              ggtitle("Illegal Immigration Arrests at Northern Border") +
+              theme_minimal()
+ggplotly(north)
+
+southwest <- ggplot(filter(by_border, Border == "Southwest"), aes(x = Year, y = Number_Arrested, fill = Type)) +
+              geom_area(alpha = 0.65, position = "dodge") +
+              xlab("Year") +
+              ylab("Total Arrests") +
+              ggtitle("Illegal Immigration Arrests at Southwest Border") +
+              theme_minimal()
+ggplotly(southwest)
+
+#while this approach was much more tedious, it made the graphs significantly easier to read
+            
 
 
 
